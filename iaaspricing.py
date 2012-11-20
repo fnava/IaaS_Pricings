dc195c22d0c3fee891cf9f9481e265db8858f055
Fortunato Navarro <fortunato.navarro@gmail.com>
2012-11-20 14:31:30 +0100

    generation of scatter 3d plots

--- iaaspricing.py
+++ iaaspricing.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 	parser.add_argument("--filter-os-type", "-fo", help="Filter results to a specific os type", choices=EC2_OS_TYPES, default=None)
 	parser.add_argument("--filter-reserve", "-fv", help="Filter results to a specific reservation", choices=RESERVATION, default=None)
 	parser.add_argument("--filter-provider", "-fp", help="Filter results to a specific provider", choices=PROVIDERS, default=None)
-	parser.add_argument("--format", "-f", choices=["json", "table", "csv", "awsgraph"], help="Output format", default="table")
+	parser.add_argument("--format", "-f", choices=["json", "table", "csv", "awsgraph", "scatter3d"], help="Output format", default="table")
 
 	args = parser.parse_args()
 
@@ -178,6 +178,8 @@ Apache license (http://www.apache.org/licenses/LICENSE-2.0.html)
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Google Visualization API Sample</title>
+
+<!-- Google graph motion chart -->
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script type="text/javascript">
 google.load('visualization', '1', {packages: ['motionchart']});
@@ -231,8 +233,96 @@ motionchart.draw(data, {'width': 800,
 google.setOnLoadCallback(drawVisualization);
 </script>
 </head>
+
+<body onload="showDemo();" style="cursor: default;">
 <body style="font-family: Arial;border: 0 none;">
-<div id="visualization" style="width: 800px; height: 600px;"></div>
+
+<div id="visualization" style="width: 800px; height: 600px;">
+</div>
 </body>
+</html>
+"""
+	elif args.format == "scatter3d":
+		from prov_aws_features import *
+		locale.setlocale(locale.LC_ALL,"C")
+		print """<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- saved from url=(0042)http://www.canvasxpress.org/scatter3d.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>Scatter3d</title>
+
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
+    
+    <meta name="keywords" content="canvasxpress, canvas, html5, graph, chart, plot, javascript, javascript library, genomic, scientific, android, animation, bar graph, line graph, dotplot, boxplot, heatmap, newick, scatter, 3d, pie, correlation, venn, network, market, candlestick, genome browser, isaac neuhaus">
+    <meta name="description" content="">
+    <meta http-equiv="Content-Language" content="en-us">
+    <meta name="Rating" content="general">
+    <meta name="googlebot" content="index,follow">
+    <meta name="robots" content="index,follow">
+    <meta name="author" content="Fortunato Navarro">
+    
+<!-- http://www.canvasxpress.org 3D scatter plot -->
+ <!--[if IE]><script type="text/javascript" src="./js/excanvas.js"></script><![endif]-->
+ <!--[if IE]><script type="text/javascript" src="./js/extext.js"></script><![endif]-->
+ <script type="text/javascript" async="" src="http://canvasxpress.org/js/ga.js">
+ </script><script type="text/javascript" src="http://canvasxpress.org/js/canvasXpress.min.js"></script>
+  <script>
+      var showDemo = function () {
+        new CanvasXpress("canvas", 
+""",
+	#Here code to generate canvasxpress scatter plot
+		cx = {
+			"x":"",
+			"y":{
+				"vars":[],
+				"smps":["GB", "GHz", "price"],
+				"desc":["intensity",],
+				"data":[]
+				},
+			"z":{
+				"Prov":[],
+				"Prod":[]	
+				},
+			"graphType": "Scatter3D",
+			"xAxis": ["GB"],
+			"yAxis": ["GHz"],
+			"zAxis": ["price"],
+			"scatterType": "false"
+		}
+		cxdate = "20121101"
+		for ds,data in dataset.items():
+			date = data["config"]["date"]	
+			if date == cxdate:
+				provider = data["config"]["provider"]
+				product = data["config"]["product"]
+				currency = data["config"]["currency"]
+				for r in data["regions"]:
+					region_name = r["region"]
+					for it in r["instanceTypes"]:
+						for term in it["prices"]:
+							hourly = it["prices"][term]["hourly"]
+							upfront = it["prices"][term]["upfront"]
+							if hourly is not None or upfront is not None:							
+								datal = [
+									features[it["type"]][mem_key],
+									features[it["type"]][cpu_key],
+									hourly
+									]
+								cx["y"]["data"].append(datal)
+								cx["y"]["vars"].append(it["type"])
+								cx["z"]["Prod"].append(product)
+								cx["z"]["Prov"].append(provider)
+		print json.dumps(cx, sort_keys=True, indent=4),
+	print """) }
+</script>
+</head>
+
+<body style="font-family: Arial;border: 0 none;">
+<div>
+<canvas id="canvas" width="800" height="600"></canvas>
+</div>
+</body>
+
 </html>
 """
--- make_graph.sh
+++ make_graph.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-python iaaspricing.py \
+ python iaaspricing.py \
     --filter-provider Amazon \
     --filter-reserve ondemand \
     --filter-os linux \
     --filter-region us-east-1 \
     -f awsgraph > graph_aws_ondemand.html
 
-
+python iaaspricing.py \
+    --filter-provider Amazon \
+    --filter-reserve ondemand \
+    --filter-os linux \
+    --filter-region us-east-1 \
+    -f scatter3d > graph_aws_scatter3d.html
