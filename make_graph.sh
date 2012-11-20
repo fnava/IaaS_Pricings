7e14051fc364242c1455a349ccc8885f1eb4fa6f
Fortunato Navarro <fortunato.navarro@gmail.com>
2012-11-21 00:37:12 +0100

    fixed line feeds

--- make_graph.sh
+++ make_graph.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 python iaaspricing.py \
-    --filter-provider Amazon \
-    --filter-reserve ondemand \
-    --filter-os linux \
-    --filter-region us-east-1 \
-    -f awsgraph > graph_aws_ondemand.html
+--filter-provider Amazon \
+--filter-reserve ondemand \
+--filter-os linux \
+--filter-region us-east-1 \
+-f awsgraph > graph_aws_ondemand.html
 
 python iaaspricing.py \
-    --filter-provider Amazon \
-    --filter-reserve ondemand \
-    --filter-os linux \
-    --filter-region us-east-1 \
-    -f scatter3d > graph_aws_scatter3d.html
+--filter-provider Amazon \
+--filter-reserve ondemand \
+--filter-os linux \
+--filter-region us-east-1 \
+-f scatter3d > graph_aws_scatter3d.html
